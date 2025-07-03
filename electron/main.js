e8cfe09c9e4a284dbd71b27e6c491298bc7169a4
Luis Fellipe Amaro <luis.fellipe@paytrack.com.br>
2025-07-03 19:59:21 -0300

    configuração do electron

--- electron/main.js
+++ electron/main.js
@@ -1,10 +1,11 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow } =  require("electron");
 const path = require("path");
 
 function createWindow() {
     const win = new BrowserWindow({
         width: 1024,
         height: 768,
+        icon: path.join(__dirname, "plant.svg"),
         webPreferences: { preload: path.join(__dirname, "preload.js") },
     });
     if (!app.isPackaged) {
